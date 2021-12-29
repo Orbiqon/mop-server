1f4fd8172f2b1eb03a6049a7828d64618d5c72f7
yasir101 <iamyasiramjad@gmail.com>
2021-12-29 18:15:57 +0500

    revoke token

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -5,6 +5,23 @@ module Api
     class TokensController < Doorkeeper::TokensController
       before_action :check_if_account_is_registered, only: :create
 
+      def revoke
+        # The authorization server responds with HTTP status code 200 if the client
+        # submitted an invalid token or the token has been revoked successfully.
+        if token.blank?
+          render json: {}, status: 200
+        # The authorization server validates [...] and whether the token
+        # was issued to the client making the revocation request. If this
+        # validation fails, the request is refused and the client is informed
+        # of the error by the authorization server as described below.
+        elsif authorized?
+          revoke_token
+          render json: {}, status: 200
+        else
+          render json: revocation_error_response, status: :forbidden
+        end
+      end
+
       private
 
       def check_if_account_is_registered
