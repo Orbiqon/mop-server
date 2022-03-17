8a1e50d57a5db3c80ecad11f271d7b5180edbbc6
Shazim Khan <iamshazimkhan@gmail.com>
2022-03-17 09:10:05 +0500

    Changes Pushed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,12 @@
 Dir[Rails.root.join('db', 'seeds', '*.rb')].sort.each do |file|
   require file
 end
+
+
+Package.create(name: 'Starter', description: 'A beginner Package to get your work seen and sell your work', enable: true, price: 8, duration_type: :monthly, duration_span: 1)
+Package.create(name: 'Plus', description: 'A advance Package to get your work seen and sell your work', enable: true, price: 15, duration_type: :monthly, duration_span: 1)
+Package.create(name: 'Pro', description: 'A premium Package to get your work seen and sell your work', enable: true, price: 25, duration_type: :monthly, duration_span: 1)
+
+Package.create(name: 'Starter', description: 'A beginner Package to get your work seen and sell your work', enable: true, price: 80, duration_type: :yearly, duration_span: 1)
+Package.create(name: 'Plus', description: 'A advance Package to get your work seen and sell your work', enable: true, price: 150, duration_type: :yearly, duration_span: 1)
+Package.create(name: 'Pro', description: 'A premium Package to get your work seen and sell your work', enable: true, price: 250, duration_type: :yearly, duration_span: 1)
