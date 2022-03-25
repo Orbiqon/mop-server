2a04c4240ee9efaaea88756f36f4eb6c27a51aac
yasir101 <iamyasiramjad@gmail.com>
2022-03-25 18:07:32 +0500

    sideqik extension disabled

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,7 +4,7 @@ Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'] }
 end
 
-Sidekiq::Extensions.enable_delay!
+# Sidekiq::Extensions.enable_delay!
 
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URL'] }
