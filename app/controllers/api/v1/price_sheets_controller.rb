4ab31f5181b63cc6f60ecb0409df9329bf4fa244
Yasir Amjad <iamyasiramjad@gmail.com>
2022-06-30 10:08:43 +0500

    update api for price Sheet

--- app/controllers/api/v1/price_sheets_controller.rb
+++ app/controllers/api/v1/price_sheets_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class PriceSheetsController < ApiController
-      
+      before_action :set_price_sheet, only: %i[update]
       def index
         @price_sheets = PriceSheet.all  
       end
@@ -15,6 +15,10 @@ module Api
         @price_sheet = PriceSheet.find_by(id: params[:id])  
       end
       
+      def update
+        @price_sheet.update!(price_sheet_params)
+      end
+      
       def get_pricing_data
         @sizes = Size.all
         @papers = Paper.all
@@ -25,6 +29,10 @@ module Api
       def price_sheet_params
         params.require(:price_sheet).permit(:name,
                                             price_sheet_entries_attributes: %i[id paper_one paper_two size price])
+      end
+      
+      def set_price_sheet
+        @price_sheet = PriceSheet.find_by(id: params[:id])
       end
     end
   end
---
+++ app/views/api/v1/price_sheets/update.json.jbuilder
@@ -0,0 +1,9 @@
+json.id @price_sheet.id
+json.name @price_sheet.name
+json.entries @price_sheet.price_sheet_entries do |entry|
+  json.id entry.id
+  json.paper_one Paper.find_by(id: entry.paper_one)&.name 
+  json.paper_two Paper.find_by(id: entry.paper_two)&.name
+  json.size Size.find_by(id: entry.size)&.name
+  json.price entry.price
+end
