7c527f5832ca6e59308acfa8be650ea2b7cd650f
Yasir Amjad <iamyasiramjad@gmail.com>
2022-08-05 15:23:24 +0500

    hahah

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,6 +1,5 @@
 class OrderMailer < ApplicationMailer
   def completed_order(id)
-    byebug
     @order = Order.find_by(id: id)
     mail(to: "yasiramjad77@gmail.com", subject: "Order Submited")
     # mail(to: @order.user.email, subject: "Order Submited")
