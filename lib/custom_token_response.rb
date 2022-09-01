cd19199b09c8494f8b891b04540bb6450e722a83
Yasir Amjad <iamyasiramjad@gmail.com>
2022-09-01 12:08:29 +0500

    login response updated

--- lib/custom_token_response.rb
+++ lib/custom_token_response.rb
@@ -5,6 +5,7 @@ module CustomTokenResponse
     user = User.find(@token.resource_owner_id)
     super.merge({
                   user: user,
+                  first_name: user.profile.first_name.present? ? user.profile.first_name : nil,
                   subscription: user.subscriptions.any?
                 })
   end
